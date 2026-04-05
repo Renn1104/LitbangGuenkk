90ae905344346e7164ec501f911b857d00800946
Rendy <rendy.pramudya30@gmail.com>
2026-04-05 20:35:48 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./pages/**/*.html",
+    "./components/**/*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
